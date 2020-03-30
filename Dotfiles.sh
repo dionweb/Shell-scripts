7509200cee0bfc7cf7c8124f6fe850c28f254f12
dionweb <dionysiostsentidis@gmail.com>
2020-03-30 18:25:15 +0200

    Dotfiles

---
+++ Dotfiles.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+echo "Download .dotfiles..."
+
+
+git clone --bare https://github.com/dionweb/Dotfiles $HOME/.dotfiles
+function dotfile {
+   /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+}
+
+mkdir -p .config-backup
+dotfile checkout
+
+if [ $? = 0 ]; then
+  echo "Download completed!";
+  else
+    echo "Backup existing .dotfiles";
+    dotfile checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+fi;
+
+dotfile checkout
+dotfile config status.showUntrackedFiles no
